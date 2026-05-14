8689218ae865abded7c0a881dff1505844472e7f
jim-junior <jimjunior854@gmail.com>
2026-05-14 10:36:02 +0300

    test deployment

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <h1 className="text-4xl font-bold">Hello, World!!</h1>
     </div>
   );
 };
